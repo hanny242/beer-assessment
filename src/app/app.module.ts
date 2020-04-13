12860672d38e6528ddc755a8aadf3fee9a7f403a
hanny242 <hannahdhawes@gmail.com>
2020-04-13 22:19:53 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeersComponent } from './beers/beers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeersComponent
   ],
   imports: [
     BrowserModule,
