099c9d2b96e574a5ed0bf82fd28f2f8e4ab1dfeb
hanny242 <hannahdhawes@gmail.com>
2020-04-14 16:58:51 +0200

    added brewery service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeersComponent } from './beers/beers.component';
+
+import { BreweryService } from './brewery.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,10 @@ import { BeersComponent } from './beers/beers.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BreweryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/beers.ts
@@ -0,0 +1,8 @@
+export interface Beer {
+    id: number;
+    name: string;
+    country: string;
+    brand: string;
+    type: string;
+    picture: string;
+}
